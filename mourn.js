5ffb00675bf2c5dc711332042feb635ce6a9a43c
Phruek Kosonsuwiwat <phruek1412@gmail.com>
2016-10-17 00:49:34 +0700

    add button

--- mourn.js
+++ mourn.js
@@ -2,7 +2,7 @@
   	$.mourn = function(options) {  		
   		var settings = $.extend({
             ribbon: true,
-            ribbonPosition: 'bottom-left',
+            ribbonPosition: 'top-right',
             gray: true,
             grayscale:0.9
       }, options);
@@ -11,44 +11,22 @@
       if(settings.gray){
         grayscale = settings.grayscale*100+'%'; 
         var styles = {
-          'filter' : 'gray',
+         'filter' : 'gray',
           'filter': 'grayscale('+grayscale+')',
           '-moz-filter': 'grayscale('+grayscale+')',
           '-webkit-filter': 'grayscale('+grayscale+')'
         };        
         $('html').css(styles);        
       }
+      
+      if(settings.ribbonPosition=='top-right'){
+        // setting css
+      }else if(settings.ribbonPosition=='top-left'){
+      	
+      }else if(settings.ribbonPosition=='bottom-right'){
 
-      if(settings.ribbon){
-        $('body').append('<div class="mourn-ribbon">x</div>');
-        if(settings.ribbonPosition=='top-right'){
-          // setting css          
-          var styles = {
-            'position' : 'fixed',
-            'right': 0,
-            'top': 0
-          }; 
-          $('.mourn-ribbon').css(styles);
-        }else if(settings.ribbonPosition=='top-left'){
-          var styles = {
-            'position' : 'fixed',
-            'left': 0,
-            'top': 0
-          };           
-        }else if(settings.ribbonPosition=='bottom-right'){
-          var styles = {
-            'position' : 'fixed',
-            'bottom': 0,
-            'right': 0
-          };
-        }else if(settings.ribbonPosition=='bottom-left'){
-          var styles = {
-            'position' : 'fixed',
-            'bottom': 0,
-            'left': 0
-          };
-        }
-        $('.mourn-ribbon').css(styles);
-      }      
+      }else if(settings.ribbonPosition=='bottom-left'){
+
+      }
   	};  	
 }(jQuery));
