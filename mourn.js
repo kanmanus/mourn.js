b361539454728c38b7aa05b69844455caec14daa
Songklod Saengvoratip <songklodsaengvoratip@Songklods-MacBook-Air.local>
2016-10-17 00:34:36 +0700

    - add gray scale function

--- mourn.js
+++ mourn.js
@@ -2,9 +2,31 @@
   	$.mourn = function(options) {  		
   		var settings = $.extend({
             ribbon: true,
-            ribbonPosition: 'top-right'
-        }, options);
-  		console.log(settings);
-  		return 'test';
+            ribbonPosition: 'top-right',
+            gray: true,
+            grayscale:0.9
+      }, options);
+      
+      // set gray scale
+      if(settings.gray){
+        grayscale = settings.grayscale*100+'%'; 
+        var styles = {
+         'filter' : 'gray',
+          'filter': 'grayscale('+grayscale+')',
+          '-moz-filter': 'grayscale('+grayscale+')',
+          '-webkit-filter': 'grayscale('+grayscale+')'
+        };        
+        $('html').css(styles);        
+      }
+
+      if(settings.ribbonPosition=='top-right'){
+        // setting css
+      }else if(settings.ribbonPosition=='top-left'){
+
+      }else if(settings.ribbonPosition=='bottom-right'){
+
+      }else if(settings.ribbonPosition=='bottom-left'){
+
+      }
   	};  	
 }(jQuery));
