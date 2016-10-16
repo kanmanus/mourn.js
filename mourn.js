488cd5ca26942fdaf90c258dee5ad667d4e30b8e
Songklod Saengvoratip <songklodsaengvoratip@Songklods-MacBook-Air.local>
2016-10-17 00:42:59 +0700

    - add ribbon function

--- mourn.js
+++ mourn.js
@@ -2,7 +2,7 @@
   	$.mourn = function(options) {  		
   		var settings = $.extend({
             ribbon: true,
-            ribbonPosition: 'top-right',
+            ribbonPosition: 'bottom-left',
             gray: true,
             grayscale:0.9
       }, options);
@@ -11,7 +11,7 @@
       if(settings.gray){
         grayscale = settings.grayscale*100+'%'; 
         var styles = {
-         'filter' : 'gray',
+          'filter' : 'gray',
           'filter': 'grayscale('+grayscale+')',
           '-moz-filter': 'grayscale('+grayscale+')',
           '-webkit-filter': 'grayscale('+grayscale+')'
@@ -19,14 +19,36 @@
         $('html').css(styles);        
       }
 
-      if(settings.ribbonPosition=='top-right'){
-        // setting css
-      }else if(settings.ribbonPosition=='top-left'){
-
-      }else if(settings.ribbonPosition=='bottom-right'){
-
-      }else if(settings.ribbonPosition=='bottom-left'){
-
-      }
+      if(settings.ribbon){
+        $('body').append('<div class="mourn-ribbon">x</div>');
+        if(settings.ribbonPosition=='top-right'){
+          // setting css          
+          var styles = {
+            'position' : 'fixed',
+            'right': 0,
+            'top': 0
+          }; 
+          $('.mourn-ribbon').css(styles);
+        }else if(settings.ribbonPosition=='top-left'){
+          var styles = {
+            'position' : 'fixed',
+            'left': 0,
+            'top': 0
+          };           
+        }else if(settings.ribbonPosition=='bottom-right'){
+          var styles = {
+            'position' : 'fixed',
+            'bottom': 0,
+            'right': 0
+          };
+        }else if(settings.ribbonPosition=='bottom-left'){
+          var styles = {
+            'position' : 'fixed',
+            'bottom': 0,
+            'left': 0
+          };
+        }
+        $('.mourn-ribbon').css(styles);
+      }      
   	};  	
 }(jQuery));
